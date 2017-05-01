81327dd80ceff983597ece91385600e4827f83e7
Kevin <walchko@users.noreply.github.com>
2017-04-30 21:13:21 -0600

    added motd

--- linux-setup.sh
+++ linux-setup.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
+# check if we are root
+if [ "$EUID" -eq 0 ]
+  then echo "Please run as pi"
+  exit 1
+fi
+
+rm -f ~/.bashrc
 ln -s ~/.bashrc ~/github/dotfiles/linux_bashrc
+
+if [ -f "/etc/profile.d/sshpwd.sh" ]; then
+  rm -f /etc/profile.d/sshpwd.sh
+else
+  echo "already removed sshpwd.sh"
+fi
+
+if [ ! -f "/etc/profile.d/motd.sh" ]; then
+  ln -s /home/pi/github/mote/software/motd /etc/profile.d/motd.sh
+else
+  echo "already setup motd.sh"
+fi
