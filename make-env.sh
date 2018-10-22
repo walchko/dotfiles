8b5ac35311063718f046d5fca053ed124994757a
walchko <walchko@users.noreply.github.com>
2018-10-21 21:44:48 -0600

    virtual env

---
+++ make-env.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+if [[ $# -ne 1 ]] ; then
+  echo "Please supply a virtual env name"
+  echo "$0 <name>"
+  exit 1
+fi
+
+export WORKON_HOME=$HOME/.virtualenvs
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+source /usr/local/bin/virtualenvwrapper.sh
+
+# delete existing
+rmvirtualenv $1
+
+# create new one
+mkvirtualenv $1
+workon $1
+./setup-python-virtenv.sh
+deactivate
--- setup-python-virtenv.sh
+++ setup-python-virtenv.sh
@@ -1,7 +1,51 @@
 #!/bin/bash
+# ------------------------------------
+# Setup a virtual environment with libraries
+# I commonly use.
+# ------------------------------------
 
-LIBS="\
-pyserial \
+LIBS="
+pyserial
+jupyter
+matplotlib
+numpy
+imutils
+opencv-contrib-python
+scikit-learn
+scikit-image
+keras
+pyzmq
+msgpack
+nose
+psutil
+pyyaml
+simplejson
+twine
+squaternion
 build_utils"
 
 pip install -U ${LIBS}
+
+# link in my folders
+BASE="/Users/kevin/github/"
+DIRS="
+ins_nav
+pygecko
+pydar
+sslam
+the-collector
+"
+
+for f in ${DIRS}
+do
+    FULL="${BASE}${f}"
+    if [ -d "${FULL}" ]; then
+        cd ${FULL}
+        echo ">> Link: ${FULL}"
+        pip --quiet install -e .
+    fi
+done
+
+echo " "
+
+pip list
