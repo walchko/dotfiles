c2f10e5b6f5f413de1da0cc9b0e7b9a33601462d
walchko <walchko@users.noreply.github.com>
2020-12-20 13:01:23 -0500

    Update venv.sh

--- venv.sh
+++ venv.sh
@@ -1,15 +1,56 @@
 #!/bin/bash
+# MIT License, Kevin Walchko, 20 Dec 2020
+
+set -e
 
 # python
-pip install -U pip setuptools wheel
+pip install -U pip setuptools wheel pytest psutil colorama attrs twine
 
 # basic science stuff
 pip install -U jupyter matplotlib numpy simplejson pyyaml
 
-pip install -U opencv-python pillow pyheif-pillow-opener
+# image stuff
+pip install -U opencv-python imageio pillow pyheif-pillow-opener
 
 # machine learning
-pip install -U scikit-learn scipy
+pip install -U scikit-learn scipy pandas
 
 # robotics
-pip install -U pyserial pyzmq
+pip install -U pyserial pyzmq sympy
+
+# web page
+pip install -U Jinja2
+
+# docker
+pip install -U docker-compose
+
+# handle my libraries
+setup_mine ()
+{
+    if pip list | grep ${1}; then
+        echo ">> ${1} is already installed"
+        return
+    fi
+    if [ -d ~/github/${1} ]; then
+        cd ~/github/${1}
+        echo "------------------------------------"
+        if [ -f "poetry.lock" ]; then
+            echo ">> found lock, rm ${1}/poetry.lock"
+            rm -f poetry.lock
+        fi
+        echo ">> installing ${1}"
+        poetry install
+        cd ..
+    else
+        echo "*** ${1} is NOT found ***"
+    fi
+}
+
+setup_mine slurm
+setup_mine squaternion
+setup_mine numpy_camera
+setup_mine opencv_camera
+setup_mine ins_nav
+setup_mine pycreate2
+setup_mine pyrk
+setup_mine the-collector
