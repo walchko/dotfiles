08cbbdc0f7b0551bec04a9577749d91699906b38
walchko <walchko@users.noreply.github.com>
2020-12-20 12:02:32 -0500

    Create venv.sh

---
+++ venv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# python
+pip install -U pip setuptools wheel
+
+# basic science stuff
+pip install -U jupyter matplotlib numpy simplejson pyyaml
+
+pip install -U opencv-python pillow pyheif-pillow-opener
+
+# machine learning
+pip install -U scikit-learn scipy
+
+# robotics
+pip install -U pyserial pyzmq
