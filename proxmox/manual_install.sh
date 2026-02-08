40fae8e84ae4a41b6412f3f8f45e62d5a2c78379
walchko <walchko@users.noreply.github.com>
2026-02-08 15:51:26 -0500

    fix path

--- proxmox/manual_install.sh
+++ proxmox/manual_install.sh
@@ -5,7 +5,7 @@ echo "<<< Start >>>"
 # Bashrc
 echo " - bashrc"
 rm -f ${HOME}/.bashrc
-ln -s ./bashrc ${HOME}/.bashrc
+ln -s ${PWD}/bashrc ${HOME}/.bashrc
 
 # debian - MOTD
 echo " - motd"
@@ -14,13 +14,13 @@ apt install -y lsb-release figlet
 rm -f /etc/motd            # remove existing one
 touch /etc/motd            # create empty file
 rm -f /etc/update-motd.d/* # remove all motd
-chmod 744 01-custom        # set permissions
-chown root:root 01-custom  # set ownership
-ln -s ./01-custom /etc/update-motd.d/01-custom
+chmod 744 ./01-custom        # set permissions
+chown root:root ./01-custom  # set ownership
+ln -s ${PWD}/01-custom /etc/update-motd.d/01-custom
 
 # setup ssh
 echo " - ssh"
 rm -f ${HOME}/.ssh/authorized_keys
-ln -s ./authorized_keys ${HOME}/.ssh/authorized_keys
+ln -s ${PWD}/authorized_keys ${HOME}/.ssh/authorized_keys
 
 echo "<<< Done >>>"
