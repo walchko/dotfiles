845b1122f55799af9761d3ab31467f7287b15edd
walchko <walchko@users.noreply.github.com>
2026-02-08 15:45:12 -0500

    added proxmox scripts

---
+++ proxmox/manual_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "<<< Start >>>"
+
+# Bashrc
+echo " - bashrc"
+rm -f ${HOME}/.bashrc
+ln -s ./bashrc ${HOME}/.bashrc
+
+# debian - MOTD
+echo " - motd"
+apt update
+apt install -y lsb-release figlet
+rm -f /etc/motd            # remove existing one
+touch /etc/motd            # create empty file
+rm -f /etc/update-motd.d/* # remove all motd
+chmod 744 01-custom        # set permissions
+chown root:root 01-custom  # set ownership
+ln -s ./01-custom /etc/update-motd.d/01-custom
+
+# setup ssh
+echo " - ssh"
+rm -f ${HOME}/.ssh/authorized_keys
+ln -s ./authorized_keys ${HOME}/.ssh/authorized_keys
+
+echo "<<< Done >>>"
