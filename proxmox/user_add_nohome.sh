c0962a65645c4c63ca561dcbf8608587f39e2ce4
walchko <walchko@users.noreply.github.com>
2026-02-09 07:59:10 -0500

    update

--- proxmox/user_add_nohome.sh
+++ proxmox/user_add_nohome.sh
@@ -13,6 +13,14 @@ PASSWD=$2
 # -M don't create home
 # -U add group with same name
 useradd ${USER} -s /bin/bash -M -U
-chpasswd << 'END'
-${USER}:${PASSWD}
-END
+# chpasswd << 'END'
+# ${USER}:${PASSWD}
+# END
+echo "${USER}:${PASSWD}" | chpasswd
+
+# Check the exit status of the chpasswd command
+if [ "$?" -eq 0 ]; then
+    echo "Password for user '${USER}' has been updated."
+else
+    echo "Failed to update password for user '${USER}'."
+fi
